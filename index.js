93e485dbe35117f500219e5ef49bf8a5c06d51f4
Sahil Vishwakarma <sahilvishwa2108@gmail.com>
2023-10-21 21:43:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var typed = new Typed("#element1", {
+    strings: ["Turning Visions into Reality","Your Vision, Our Expertise","Bringing Ideas to Life, Together"],
+    typeSpeed: 80,
+    loop:true,
+    showCursor:false
+});
---
+++ script.js
@@ -0,0 +1,26 @@
+
+function scrollToHome(){
+    $('html, body').animate({
+         scrollTop: $('#home').offset().top
+    }, 500);
+}
+// function scrollToServices(){
+// 	$('html, body').animate({
+//          scrollTop: $('#services').offset().top
+//     }, 500);
+// }
+// function scrollToAbout(){
+// 	$('html, body').animate({
+//          scrollTop: $('#about').offset().top
+//     }, 500);
+// }
+// function scrollToCOntactk(){
+// 	$('html, body').animate({
+//          scrollTop: $('#contact').offset().top
+//     }, 500);
+// }
+// function scrollToWork(){
+// 	$('html, body').animate({
+//          scrollTop: $('#work').offset().top
+//     }, 500);
+// }
